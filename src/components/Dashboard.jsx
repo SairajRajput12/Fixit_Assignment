f42e7071192c01da301ac4eb122c4828a740cb99
SairajRajput12 <125473711+SairajRajput12@users.noreply.github.com>
2025-01-05 02:43:13 +0530

    added changes

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -4,10 +4,11 @@ import './Dashboard.css'
 import SideBoard from './SideBoard'
 import Button from '../UI/Button'
 import ShowQuiz from './ShowQuiz'
+import { useNavigate } from 'react-router-dom'
 
 export default function Dashboard() {
   const [selectTab,setSelectTab] = useState('create');
-
+  const navigate = useNavigate(); 
   let content = null; 
   if(selectTab == 'create'){
     content = <CreateQuiz />
@@ -16,12 +17,16 @@ export default function Dashboard() {
     content = <ShowQuiz />
   }
 
+  const handleSignout = () => {
+    navigate('/');
+  }
 
   return (
     <div className='dashboard'>
       <SideBoard>
           <Button onClick={() => setSelectTab('create')} className='project-button' >Create Quiz</Button>
           <Button onClick={() => setSelectTab('your')} className='project-button' >Your Quiz's</Button>
+          <Button onClick={handleSignout} className='project-button' >Signout</Button>
       </SideBoard>
       {content}
     </div>
